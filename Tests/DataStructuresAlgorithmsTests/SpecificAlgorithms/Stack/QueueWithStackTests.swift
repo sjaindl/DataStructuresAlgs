93c18c5380f15806260c0c29f7f145b09b526e1e
Stefan Jaindl <jaindl.stefan@gmail.com>
2020-06-21 23:01:05 +0200

    Implement Queue with Stacks

---
+++ Tests/DataStructuresAlgorithmsTests/SpecificAlgorithms/Stack/QueueWithStackTests.swift
@@ -0,0 +1,28 @@
+//
+//  MultiStackTests.swift
+//  
+//
+//  Created by Stefan Jaindl on 14.06.20.
+//
+
+import Foundation
+
+import XCTest
+@testable import DataStructuresAlgorithms
+
+open class QueueWithStackTests: XCTestCase {
+    
+    open func testQueueWithStacks() throws {
+        let queue = QueueWithStacks<Int>()
+        
+        try queue.enqueue(value: 1)
+        try queue.enqueue(value: 2)
+        try queue.enqueue(value: 3)
+        
+        XCTAssertFalse(queue.isEmpty())
+        XCTAssertEqual(try queue.dequeue(), 1)
+        XCTAssertEqual(try queue.dequeue(), 2)
+        XCTAssertEqual(try queue.dequeue(), 3)
+        XCTAssertTrue(queue.isEmpty())
+    }
+}
