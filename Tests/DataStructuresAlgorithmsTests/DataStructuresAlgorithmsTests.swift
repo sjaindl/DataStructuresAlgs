754e6438a1333d8ea73a5a0d92397ca0e8599842
Stefan Jaindl <jaindl.stefan@gmail.com>
2020-06-30 23:55:47 +0200

    Implement FirstCommonAncestor

--- Tests/DataStructuresAlgorithmsTests/DataStructuresAlgorithmsTests.swift
+++ Tests/DataStructuresAlgorithmsTests/DataStructuresAlgorithmsTests.swift
@@ -74,5 +74,6 @@ open class DataStructuresAlgorithmsTests: XCTestCase {
         NodeSuccessorTests().testNodeSuccessorCaseOne()
         NodeSuccessorTests().testNodeSuccessorCaseTwo()
         NodeSuccessorTests().testNodeSuccessorCaseThree()
+        FirstCommonAncestorTests().testFirstCommonAncestor()
     }
 }
