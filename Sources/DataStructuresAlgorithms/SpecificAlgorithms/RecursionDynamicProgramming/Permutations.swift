ae3b64bffe4fabae29ecdbc04396afb17e50d7da
Stefan Jaindl <stefan.jaindl@alturos.com>
2020-08-25 23:58:43 +0200

    Implement Permutations with duplicates

--- Sources/DataStructuresAlgorithms/SpecificAlgorithms/RecursionDynamicProgramming/Permutations.swift
+++ Sources/DataStructuresAlgorithms/SpecificAlgorithms/RecursionDynamicProgramming/Permutations.swift
@@ -35,4 +35,44 @@ open class Permutations {
         
         return permutations
     }
+    
+    open func permutationsWithDuplicates(of string: String) -> [String] {
+        return permutations(of: String(string.sorted()))
+    }
+    
+    private func permutations(of string: String) -> [String] {
+        if string.count <= 1 {
+            return [string]
+        }
+        
+        var perms: [String] = []
+        
+        for (index, char) in string.enumerated() {
+            if index > 0, string[string.index(string.startIndex, offsetBy: index - 1)] == char {
+                continue //char is a duplicate of previous char - so, skip duplicate
+            }
+            
+            var prefix = ""
+            var postfix = ""
+            
+            if index > 0 {
+                let prefixIndex = string.index(string.startIndex, offsetBy: index - 1)
+                prefix = String(string[string.startIndex ... prefixIndex])
+            }
+            
+            if index < string.count - 1 {
+                let postfixIndex = string.index(after: string.index(string.startIndex, offsetBy: index))
+                postfix = String(string[postfixIndex ..< string.endIndex])
+            }
+            
+            let remainder = "\(prefix)\(postfix)"
+            
+            let subPermuations = permutations(of: remainder)
+            for subPermutation in subPermuations {
+                perms.append("\(char)\(subPermutation)")
+            }
+        }
+        
+        return perms
+    }
 }
